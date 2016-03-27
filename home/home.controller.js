6ef4eddddc74b46b0263bc2562c352fb70d4dd77
bohong <justin.hong@hotmail.com>
2016-03-27 20:22:11 +0800

    json successfully passed to view page, vm.something assigned in handler directly

--- home/home.controller.js
+++ home/home.controller.js
@@ -5,19 +5,22 @@
         .module('app')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['UserService', '$rootScope'];
-    function HomeController(UserService, $rootScope) {
+    HomeController.$inject = ['UserService', '$rootScope', '$http'];
+    function HomeController(UserService, $rootScope, $http) {
         var vm = this;
 
         vm.user = null;
         vm.allUsers = [];
         vm.deleteUser = deleteUser;
+		vm.msg = null;
 
         initController();
 
         function initController() {
             loadCurrentUser();
             loadAllUsers();
+			loadProduct('http://120.25.216.99:8080/CubicCapital/finaProduct/list/1/1');
+			
         }
 
         function loadCurrentUser() {
@@ -39,6 +42,28 @@
             .then(function () {
                 loadAllUsers();
             });
+        }
+		
+		
+		// for cubic capital
+
+		
+		
+		
+		function loadProduct(url){
+			return $http.get(url).then(handleSuccess, handleError('Error getting product'));
+		}
+		
+		function handleSuccess(res) {
+			console.log(res);
+			console.log(JSON.parse(res.data));
+            vm.msg = JSON.parse(res.data);
+        }
+
+        function handleError(error) {
+            return function () {
+                return { success: false, message: error };
+            };
         }
     }
 
